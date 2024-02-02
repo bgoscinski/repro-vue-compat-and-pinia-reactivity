2ee52bcb3270116a54e41889c4179fb201344df9
Bartosz Gościński <bartosz.goscinski@gmail.com>
2024-02-02 10:40:08 +0100

    Rule out pinia

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 1. Run `npm install`
-1. Run `npm test`. Both tests fail
-1. Kill vitest
+1. Run `npm test`. Test fails
 1. Remove `'vue': '@vue/compat'` alias from `./vite.config.js`
-1. Run `npm test` again. Both tests pass 🤯
+1. Run `npm test` again. Test passes 🤯
--- repro.spec.js
+++ repro.spec.js
@@ -1,71 +1,30 @@
 import { mount } from '@vue/test-utils';
-import { createPinia, defineStore, setActivePinia } from 'pinia';
 import { expect, test } from 'vitest';
-import { defineComponent, nextTick, ref } from 'vue';
+import { defineComponent, nextTick, ref as vueRef } from 'vue';
+import { ref as demiRef } from 'vue-demi';
 
-test('maintains reactivity (composition store)', async () => {
-	const foo = ref(0);
-	const useCounterStore = defineStore('counter', () => ({
-		count: ref(0),
-	}));
-
-	const pinia = createPinia();
-	setActivePinia(pinia);
-	const store = useCounterStore();
+test('maintains reactivity', async () => {
+	const vRef = vueRef(0);
+	const dRef = demiRef(0);
 
 	const Comp = defineComponent({
 		setup() {
 			return () => {
-				return `${store.count}:${foo.value}`;
+				return `${dRef.value}:${vRef.value}`;
 			};
 		},
 	});
 
-	const wrapper = mount(Comp, { global: { plugins: [pinia] } });
+	const wrapper = mount(Comp);
 
 	expect(wrapper.text()).toEqual('0:0');
 
-	store.count++;
+	dRef.value++;
 	await nextTick();
 
 	expect(wrapper.text()).toEqual('1:0');
 
-	foo.value++;
-	await nextTick();
-
-	expect(wrapper.text()).toEqual('1:1');
-});
-
-test('maintains reactivity (options store)', async () => {
-	const foo = ref(0);
-	const useCounterStore = defineStore('counter', {
-		state: () => ({
-			count: 0,
-		}),
-	});
-
-	const pinia = createPinia();
-	setActivePinia(pinia);
-	const store = useCounterStore();
-
-	const Comp = defineComponent({
-		setup() {
-			return () => {
-				return `${store.count}:${foo.value}`;
-			};
-		},
-	});
-
-	const wrapper = mount(Comp, { global: { plugins: [pinia] } });
-
-	expect(wrapper.text()).toEqual('0:0');
-
-	store.count++;
-	await nextTick();
-
-	expect(wrapper.text()).toEqual('1:0');
-
-	foo.value++;
+	vRef.value++;
 	await nextTick();
 
 	expect(wrapper.text()).toEqual('1:1');
